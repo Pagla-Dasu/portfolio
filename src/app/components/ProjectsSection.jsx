27a2ed41cb9811c63ec189a91a5bea9e05381105
PaglaDasu <snehasishbabin2001@gmail.com>
2024-03-29 16:18:36 +0530

    Update: watchtower

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -7,6 +7,25 @@ import { motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
+    title: "Boards",
+    description:
+      "A Story Boarding web app that allows the user to jot their ideas and collaborate with other people.",
+    image: "/images/projects/6.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Pagla-Dasu/Boards",
+    previewUrl: "https://boards-pagladasu.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "WatchTower",
+    description: "A web app to make sure no one trespasses your private space.",
+    image: "/images/projects/7.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/Pagla-Dasu/Boards",
+    previewUrl: "https://baywatch.vercel.app/",
+  },
+  {
+    id: 7,
     title: "Responsive Dashboard",
     description:
       "A responsive dashboard completely made with basic HTML and CSS ans used some JS.",
@@ -16,7 +35,7 @@ const projectsData = [
     previewUrl: "https://responsive-static-dashboard.vercel.app",
   },
   {
-    id: 2,
+    id: 3,
     title: "Netflix Clone",
     description:
       "A Responsive Netflix Clone with personal login and signup page, a home page for the movies and trailers to watch. Made with TypeSript, Next.js, TailwindCSS, Redux, Firebase, and many more technologies.",
@@ -26,7 +45,7 @@ const projectsData = [
     previewUrl: "https://netflix-clone-pagla-dasu.vercel.app",
   },
   {
-    id: 3,
+    id: 4,
     title: "Disney+ Clone",
     description:
       "This is a clone of the famous hotstar app with google authentication, firebase and an responsive UI.",
@@ -36,7 +55,7 @@ const projectsData = [
     previewUrl: "https://disney-clone-pagladasu.vercel.app/",
   },
   {
-    id: 4,
+    id: 6,
     title: "Tesla Clone",
     description:
       "A responsive clone website of Tesla's original website, to showcase basic reactJs skills.",
@@ -54,16 +73,6 @@ const projectsData = [
     tag: ["All", "Web"],
     gitUrl: "https://github.com/Pagla-Dasu/google-clone",
     previewUrl: "https://google-clone-pagla-dasu.vercel.app/",
-  },
-  {
-    id: 6,
-    title: "Boards",
-    description:
-      "A Story Boarding web app that allows the user to jot their ideas and collaborate with other people.",
-    image: "/images/projects/6.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/Pagla-Dasu/Boards",
-    previewUrl: "https://boards-pagladasu.vercel.app/",
   },
 ];
 
